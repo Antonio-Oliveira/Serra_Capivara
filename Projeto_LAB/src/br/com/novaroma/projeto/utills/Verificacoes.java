b6cee0bb1a79ea1aa24837d3ae00faa5a6a7885a
User <User@192.168.15.9>
2019-09-22 22:54:55 -0300

    Veriicacao do email

--- Projeto_LAB/src/br/com/novaroma/projeto/utills/Verificacoes.java
+++ Projeto_LAB/src/br/com/novaroma/projeto/utills/Verificacoes.java
@@ -1,10 +1,28 @@
 package br.com.novaroma.projeto.utills;
 import br.com.novaroma.projeto.entidades.Usuario;
 
-public interface Verificacoes {
+public  class Verificacoes {
+	public static void main(String[]args) {
+
+		String email = "melyssagoliveiragmail.com";
+		int indiceEmail = email.indexOf('@');
+		if (indiceEmail > 0) {
+		System.out.println("E-mail válido!");
+	}else {
+		System.out.println("E-mail inválido!");
+		
+	}
+	}
+}
+
+
+
+
+	
 	
 	//public boolean verificarCPF(Usuario usuario);
-	//public boolean verificarEmail(Usuario usuario);
 	//public boolean verificarSenha(Usuario usuario);
+	
+	
 
-}
+
